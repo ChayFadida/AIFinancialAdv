45f2b69d6e04c887dccba5cfb978cc599cfe08b5
ChayFadida <chayfadida1997@gmail.com>
2025-01-29 17:29:02 +0200

    fix bug with already in progress

--- stockReports/utils/types/report_types.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum, unique
-
-@unique
-class ReportType(Enum):
-    WEB_REPORT = "web_report"
-    QK_REPORT = "qk_report"
-    BOTH = "both"
-    def __str__(self):
-        return self.name
